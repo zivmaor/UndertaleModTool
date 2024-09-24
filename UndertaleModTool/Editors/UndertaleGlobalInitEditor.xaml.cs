e4c70d36ee5a601b710e3a7d26447d0139dfe656
zivmaor <ziv.maor06@gmail.com>
2024-09-24 03:25:38 +0300

    fix GlobalInitEditor resizing

--- UndertaleModTool/Editors/UndertaleGlobalInitEditor.xaml.cs
+++ UndertaleModTool/Editors/UndertaleGlobalInitEditor.xaml.cs
@@ -20,9 +20,19 @@ namespace UndertaleModTool
     /// </summary>
     public partial class UndertaleGlobalInitEditor : DataUserControl
     {
+        public const double MinScriptsGridHeight = 100;
+
         public UndertaleGlobalInitEditor()
         {
             InitializeComponent();
+        }
+
+        private void UndertaleGlobalInitEditor_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Calculates the min height of the Editor, so it won't resize any smaller.
+            // At min height all fields and at least a few lines of scripts grid should be displayed.
+            // Must be done at runtime since the size of fields is unknown until they are rendered.
+            this.MinHeight = FieldsGrid.ActualHeight + MinScriptsGridHeight;
         }
     }
 }
